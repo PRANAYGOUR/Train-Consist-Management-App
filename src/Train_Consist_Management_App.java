1e9687e41ff45ca1502129993e30f20040d83e0c
PRANAYGOUR <pranaygour3042007@gmail.com>
2026-03-25 09:19:09 +0530

    UC1 implementation

--- src/Train_Consist_Management_App.java
+++ src/Train_Consist_Management_App.java
@@ -3,7 +3,11 @@ import java.util.List;
 
 public class Train_Consist_Management_App {
     public static void main(String[] args){
-        System.out.println("Base Application Setup");
+        System.out.println("=================================\n");
+        System.out.println("Train Consist Management App \n");
+        System.out.println("=================================\n");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train initialized Successfully....");
 
     }
 }

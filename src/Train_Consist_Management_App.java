5444103f1ab605d3bf0a802a49fca2d5d4317040
PRANAYGOUR <pranaygour3042007@gmail.com>
2026-03-25 09:11:26 +0530

    Base application setup

---
+++ src/Train_Consist_Management_App.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Train_Consist_Management_App {
+    public static void main(String[] args){
+        System.out.println("Base Application Setup");
+
+    }
+}
